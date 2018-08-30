02e09a31cf97261ebb63f08ea1f797711ec74c00
徐国平 <xuguoping@ppdai.com>
2018-08-30 20:08:11 +0800

    redis

--- happy-springboot/happy-springboot-service/src/main/java/com/happy/springboot/service/TestGit.java
+++ happy-springboot/happy-springboot-service/src/main/java/com/happy/springboot/service/TestGit.java
@@ -2,6 +2,6 @@ package com.happy.springboot.service;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.print("v-redis");
+        System.out.print("v-redis2");
     }
 }
